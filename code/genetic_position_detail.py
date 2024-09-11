16fd843cd6e3bce8d180e4ff3d283b4c197a5877
Georges Kanaan <georges@gkanaan.com>
2024-09-10 23:24:15 -0500

    path fix

--- code/genetic_position_detail.py
+++ code/genetic_position_detail.py
@@ -115,7 +115,7 @@ def run_analysis(genome_name, coverage, data_dir, fig_savepath="plots"):
 if __name__ == "__main__":
     for coverage in ["5", "5_agg"]:
         print(f"Running rao analysis at coverage {coverage}")
-        data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"../data/methylation_data/methylation_{coverage}")
+        data_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"../../methylation_data/methylation_{coverage}")
         for genome in os.listdir(data_dir):
             if genome == ".DS_Store":
                 continue
