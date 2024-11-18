5c45f05876b4018fc21df7c49666e733f017a71d
Gio Kanaan <georges@gkanaan.com>
2024-11-17 21:34:33 -0800

    Update barnacle_manager.py

--- src/barnacle_manager.py
+++ src/barnacle_manager.py
@@ -460,8 +460,8 @@ def execute_bootstrap(boot_id, model_manager, data_manager, dataset, replicate_l
 if __name__ == "__main__":
     # Paramaters
     GENOME_NAME = "Pelagibacter_r-contigs"
-    LAMBDAS = [0]#[0, 0.001, 0.01, 0.1, 1, 0.05, 0.5]  # Adjust lambdas as needed
     RANKS = list(range(1,30))
+    LAMBDAS = [0, 0.001, 0.005, 0.01, 0.1, 1, 0.05, 0.5, 0.8, 1, 2, 4] + list(np.arange(0.02, 0.6, 0.02))  # Adjust lambdas as needed
     N_BOOTSTRAPS = 27
     OUTPUT_DIR = Path(__file__).parent.resolve() / Path(f'../../data/models/{GENOME_NAME}/')
 
