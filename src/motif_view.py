1f0d8e697fb698ded1f0fe23af1a079553c9da44
Georges Kanaan <georges@gkanaan.com>
2024-12-30 17:07:50 +0100

    Update motif_view.py

--- src/motif_view.py
+++ src/motif_view.py
@@ -200,6 +200,9 @@ if __name__ == "__main__":
             Genome._Genome__methylation_data_dir = methylation_path
 
             for name in Genome.valid_genome_names():
+                if "metagenome" in name:
+                    continue
+                    
                 genome = Genome(name)
 
                 # Motif
