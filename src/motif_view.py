69091c1be536e246ed45354e5b5036317c8d4871
Georges Kanaan <georges@gkanaan.com>
2024-12-31 11:05:03 +0100

    Csv includes switched positions only

--- src/motif_view.py
+++ src/motif_view.py
@@ -29,7 +29,7 @@ def motif_methylated_frequency(genome: Genome, motif: Motif):
                              pl.col("Treatment").replace(readable_sample_name))
 
     hue_order = [readable_sample_name["top"], readable_sample_name["middle"], readable_sample_name["bottom"]]
-    fig = plt.subplots(figsize=(16, 12))
+    plt.subplots(figsize=(16, 12))
     sns.histplot(data.to_pandas(), x="Methylated", hue="Treatment", hue_order=hue_order, stat="count", multiple="dodge", element="bars")
     plt.suptitle(f"{genome.name} methylation motifs")
 
@@ -38,19 +38,6 @@ def motif_methylated_frequency(genome: Genome, motif: Motif):
         plt.show()
     else:
         plt.savefig(genome.plot_dir / "motif_methylated_frequency.pdf", format="pdf")
-
-
-def motif_view(genome: Genome, motif: Motif):
-    # Add function
-    data = genome.add_gene_caller_id(motif.data.lazy(), include_intergenic=True).collect(streaming=True)
-    gc = GeneCollection(data.get_column("gene_callers_id").unique().to_list(), genome)
-    data = data.join(gc.get_function().collect(streaming=True), on="gene_callers_id", how="left")
-
-    # Add nearest gene if not in gene
-    data = genome.nearest_gene_to_positions(data)
-
-    # Write to CSV
-    data.write_csv(genome.plot_dir / f"{motif}_motif_view.csv")
 
 
 def number_of_positions_switched(genome: Genome, motif: Motif):
@@ -85,10 +72,8 @@ def number_of_positions_switched(genome: Genome, motif: Motif):
 
     # Plot the heatmap
     plt.figure(figsize=(10, 6))
-    sns.heatmap(transition_counts, annot=True, fmt="g", cmap="coolwarm", cbar_kws={'label': 'Transition Count'})
-    plt.title(f"Transition Heatmap (Bottom vs. Top) in {genome.readable_name}")
-    plt.xlabel("Top")
-    plt.ylabel("Bottom")
+    sns.heatmap(transition_counts, annot=True, fmt="g", cmap="coolwarm", cbar_kws={'label': 'Number of positions'})
+    plt.title(f"Transition map in {genome.readable_name}")
 
     if system() == "Darwin":
         plt.show()
@@ -96,98 +81,40 @@ def number_of_positions_switched(genome: Genome, motif: Motif):
         plt.savefig(genome.plot_dir / f"motif{motif.motif}_positions_switched.pdf", format="pdf")
 
 
-def plot_genes_regions(motif: Motif, gene_collection: GeneCollection, relative_position: int = 0, relative_start: int = -40,
-                       relative_end: int = 20):
-    # Keep genes that have a start if relative_start is negative
-    keep_ids = (gene_collection.is_start_missing.filter(pl.col("partial_begin").eq(False))
-                .collect(streaming=True).get_column("gene_callers_id").to_list())
-    gene_collection = GeneCollection(keep_ids, genome)
+def annotate_switched_positions(genome: Genome, motif: Motif):
+    # Filter down to data that has switched
+    data = (motif.data
+            .filter(pl.col("Treatment").is_not_null() & (
+                pl.col(motif.meth_type).is_not_null() | pl.col(motif.canonical_base).is_not_null()))
+            .filter(pl.col("Treatment").n_unique().eq(3).over("contig", "strand", "position"))
+            .group_by("contig", "strand", "position", "Treatment")
+            .agg(pl.col(motif.meth_type).mean()))
 
-    # Get the data
-    methyl_data = gene_collection.load_flanking_methylation_data(relative_position,
-                                                                 (relative_start, relative_end),
-                                                                 common_only=True)
+    # Binarize meth_type and canonical_base values
+    data = data.with_columns((pl.col(motif.meth_type) > 0.5).alias("binarized_meth_type"))
 
-    # Filter to motifs
-    methyl_data = methyl_data.join(motif.positions.lazy(), on=["contig", "strand", "position"], how="inner")
+    # Assuming `data` is the DataFrame with the required columns
+    # Filter data for "bottom" and "top" treatments
+    bottom_data = data.filter(pl.col("Treatment").eq("bottom"))
+    top_data = data.filter(pl.col("Treatment").eq("top"))
 
-    # Get DF for all genes
-    data = methyl_data.with_columns(pl.col('sample').replace(barcode_replicate_map))
-    data = data.rename(readable_methylation_name).rename({"sample": "Sample", "position": "Position"})
-    data = data.with_columns(pl.col('Sample').replace(readable_sample_name)).collect(streaming=True)
+    # Ensure the data is aligned by some key, e.g., sample_id, to compare "bottom" and "top"
+    # Here, we assume there's a common column "sample_id" to align both treatments
+    aligned_data = bottom_data.join(top_data, on=["contig", "strand", "position"], suffix="_top")
 
-    if data.height == 0:
-        print(
-            f"No data for genes in genome {gene_collection.genome.name} in region {relative_position} +- {relative_start}, {relative_end}")
-        return
+    # Filter down to switched positions
+    switched_positions = aligned_data.filter(pl.col("binarized_meth_type").ne(pl.col("binarized_meth_type_top")))
+    
+    # Add function
+    data = genome.add_gene_caller_id(switched_positions.lazy(), include_intergenic=True).collect(streaming=True)
+    gc = GeneCollection(data.get_column("gene_callers_id").unique().to_list(), genome)
+    data = data.join(gc.get_function().collect(streaming=True), on="gene_callers_id", how="left")
 
-    # All types plot
-    long_form = (data.unpivot(on=list(readable_methylation_name.values()),
-                              index=["Sample", "Position"],
-                              variable_name="Methylation type",
-                              value_name="Normalized methylation fraction")
-                 .filter(pl.col("Normalized methylation fraction").is_not_null() &
-                         pl.col("Normalized methylation fraction").is_not_nan()))
+    # Add nearest gene if not in gene
+    data = genome.nearest_gene_to_positions(data)
 
-    # Get the most frequent nucleotide at each position
-    sequence = gene_collection.get_flanking_sequence(relative_position, (relative_start, relative_end))
-    sequence = sequence.with_columns(pl.col("sequence").str.split("")).explode("sequence")
-    sequence = sequence.with_columns(
-        (pl.cum_count("gene_callers_id") - 1 + relative_start).over("gene_callers_id").alias("position"))
-    sequence_str = sequence.group_by("position").agg(pl.col("sequence").mode().first().alias("mode"))
-    sequence_str = sequence_str.sort("position").collect().get_column("mode").str.join("").item()
-
-    # Get DF for promoter distribution
-    promoter_positions = gene_collection.pribnow_box_position_and_sequence.drop("gene_callers_id",
-                                                                                "pribnow_box_sequence").filter(
-        pl.col("pribnow_box_position").is_not_null()).collect().to_pandas()
-
-    hue_order = [readable_sample_name["top"], readable_sample_name["middle"], readable_sample_name["bottom"]]
-    fig, axes = plt.subplots(7, 1, figsize=(int((relative_end - relative_start) * 0.4), 45),
-                             layout="constrained", sharex=True)
-    fig.suptitle(f"All gene promoter methylation in {genome.readable_name}", fontsize=28)
-
-    # Promoter position distribution plot
-    sns.histplot(promoter_positions, ax=axes[0], kde=(len(promoter_positions) > 1))
-    axes[0].set_title(f"Proportion of genes with pribnox box: {len(promoter_positions)} / {len(gene_collection.ids)}")
-
-    # Per type plot
-    for i, meth_type in enumerate(readable_methylation_name.values()):
-        i += 1
-        sns.lineplot(data.to_pandas(), x="Position", y=meth_type, hue="Sample", ax=axes[i], hue_order=hue_order)
-
-    sns.lineplot(long_form.to_pandas(), x="Position", y="Normalized methylation fraction", hue="Sample",
-                 style="Methylation type", ax=axes[4], hue_order=hue_order)
-
-    # Plot number of data points at each position
-    sns.histplot(data, x="Position", hue="Sample", ax=axes[5], discrete=True, multiple="stack", hue_order=hue_order)
-
-    # Plot distribution of nucleotides
-    nucleotide_freq = sequence.select("sequence", "position").rename({"sequence": "Nucleotide"}).collect(
-        streaming=True).to_pandas()
-    sns.histplot(nucleotide_freq, x="position", hue="Nucleotide", ax=axes[6], discrete=True, multiple="stack",
-                 hue_order=["A", "T", "G", "C"], palette="Paired")
-
-    # Plot the sequence as X ticks
-    ticks = np.linspace(relative_start, relative_end, len(sequence_str))
-    axes[-1].set_xticks(ticks)
-    axes[-1].set_xticklabels(sequence_str)
-
-    # Draw line at  0
-    for ax in axes:
-        ax.axvline(x=0, color='black', linestyle='--', alpha=0.7)
-
-    # Highlight start codon
-    for i, label in enumerate(axes[-1].get_xticklabels()):
-        if 0 <= ticks[i] < 3:  # Start codon
-            label.set_color('green')
-
-    if system() == "Darwin":
-        plt.show()
-    else:
-        plt.savefig(genome.plot_dir / "all_genes.pdf", format="pdf")
-
-    return
+    # Write to CSV
+    data.write_csv(genome.plot_dir / f"{motif}_motif_view.csv")
 
 
 if __name__ == "__main__":
@@ -204,6 +131,7 @@ if __name__ == "__main__":
                     continue
 
                 genome = Genome(name)
+                print(f"Processing {genome.name}")
 
                 # Motif
                 motifs = genome.motifs
@@ -212,6 +140,11 @@ if __name__ == "__main__":
 
                     # Print percent of positions with no data
                     data = motif.data.get_column(motif.meth_type)
+                    if len(data) == 0:
+                        breakpoint()
+                        print(f"No data for {motif.motif} in any treatment")
+                        continue
+
                     for treatment in motif.data.get_column("Treatment").unique().to_list():
                         if treatment is None:
                             d = motif.data.filter(pl.col("Treatment").is_null())
@@ -221,10 +154,8 @@ if __name__ == "__main__":
                         if d.height == 0:
                             print(f"No data for {motif.motif} in {treatment}")
                             continue
-                        print(f"Motif {motif.motif} in {treatment} has {d.null_count().get_column(motif.meth_type).item() / d.height * 100:.2f}% positions with no data")
+                        print(f"Motif {motif.motif} in {treatment} has {d.null_count().get_column(motif.meth_type).item() / d.height * 100:.2f}% of positions with no data")
 
                     motif_methylated_frequency(genome, motif)
                     number_of_positions_switched(genome, motif)
-                    gene_collection = GeneCollection(genome.gene_ids, genome)
-                    plot_genes_regions(motif, gene_collection, relative_position=0, relative_start=-40, relative_end=10)
-                    motif_view(genome, motif)
+                    annotate_switched_positions(genome, motif)
--- src/objects/motif.py
+++ src/objects/motif.py
@@ -16,12 +16,12 @@ class Motif(object):
     def __init__(self, genome: Genome, motif: str):
         self.genome: Genome = genome
         self.motif: str = motif
-        self.meth_type: str = None
-        self.offset: int = None
-        self.frac_mod: float = None
-        self.high_count: int = None
-        self.low_count: int = None
-        self.mid_count: int = None
+        self.meth_type: str = ""
+        self.offset: int = -1
+        self.frac_mod: float = -1
+        self.high_count: int = -1
+        self.low_count: int = -1
+        self.mid_count: int = -1
 
         self.strings: list[str] = generate_possible_sequences(motif)
 
@@ -36,7 +36,7 @@ class Motif(object):
         motifs = pl.read_csv(str(genome._methylation_data_dir / "all_motifs-5-motifs.tsv"), separator="\t", has_header=True)
 
         # Create a Motif object for each row
-        motif_objs = []
+        motif_objs = []m
         for row in motifs.iter_rows(named=True):
             motif = Motif(genome, row["motif"])
 
@@ -60,6 +60,7 @@ class Motif(object):
         contigs = []
         positions = []
         strands = []
+        motifs = []
 
         for contig, seqrecord in self.genome.sequence.items():
             pos_strand = str(seqrecord.seq)
@@ -67,24 +68,32 @@ class Motif(object):
 
             # Find motif in positive strand
             pattern = re.compile('|'.join(map(re.escape, self.strings)))
-            pos_positions = [match.start() + self.offset for match in pattern.finditer(pos_strand)]
-            neg_positions = [match.start() + self.offset for match in pattern.finditer(neg_strand)]
+
+            pos_iter = pattern.finditer(pos_strand)
+            pos_positions = [match.start() + self.offset for match in pos_iter]
+            pos_motifs = [match[0] for match in pos_iter]
+
+            neg_iter = pattern.finditer(neg_strand)
+            neg_positions = [match.start() + self.offset for match in neg_iter]
+            neg_motifs = [match[0] for match in neg_iter]
 
             # Add to lists
             contigs.extend([contig] * len(pos_positions))
             positions.extend(pos_positions)
             strands.extend([True] * len(pos_positions))
+            motifs.extend(pos_motifs)
 
             contigs.extend([contig] * len(neg_positions))
             positions.extend(neg_positions)
             strands.extend([False] * len(neg_positions))
+            motifs.extend(neg_motifs)
 
         # Make a dataframe and return it
         return pl.DataFrame({
             "contig": contigs,
             "position": positions,
             "strand": strands,
-            "motif": [self.motif] * len(contigs)
+            "motif": motifs
         }).lazy()
 
 
@@ -101,61 +110,6 @@ class Motif(object):
         data = data.select("contig", "position", "strand", self.meth_type, self.canonical_base, "Treatment", "sample")
 
         return data.collect(streaming=True)
-
-
-def number_of_positions_switched(genome: Genome):
-    data = genome.load_all_methylation_data(normalize=True, in_every_treatment=True, treatments=["top", "bottom"])
-
-    # Make a binary decision on methylation state at a positon
-    data = data.with_columns(pl.col("sample").replace(barcode_replicate_map).alias("treatment")).sort("treatment")
-    data = data.group_by("contig", "strand", "position", "treatment").agg(pl.col(*readable_methylation_name.keys()))
-
-    # Binarize
-    data = data.with_columns(pl.col(*readable_methylation_name.keys()).list.mean() > 0.5)
-
-    for key, meth_group in base_methylation_map.items():
-        data = data.with_columns(pl.any_horizontal(*meth_group).alias(key))
-
-    # Count the number of transitions between each possibility in a treatment
-    data = data.sort("treatment").group_by("contig", "strand", "position", maintain_order=True).agg(
-        pl.col("A").alias("A_bottom"),
-        pl.col("C").alias("C_bottom"),
-        pl.col("A").shift(-1).alias("A_top"),
-        pl.col("C").shift(-1).alias("C_top")
-    ).explode(pl.col("A_bottom"), pl.col("C_bottom"), pl.col("A_top"), pl.col("C_top")).collect(streaming=True)
-
-    # Count transitions
-    Atransition_counts = data.group_by(["A_bottom", "A_top"]).len().rename({"len": "transition_count"})
-    Ctransition_counts = data.group_by(["C_bottom", "C_top"]).len().rename({"len": "transition_count"})
-
-    # Convert to pandas and then pivot
-    Atransition_counts = Atransition_counts.to_pandas().pivot(index="A_bottom", columns="A_top",
-                                                              values="transition_count")
-    Ctransition_counts = Ctransition_counts.to_pandas().pivot(index="C_bottom", columns="C_top",
-                                                              values="transition_count")
-
-    # Plot the heatmap
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(Atransition_counts, annot=True, fmt="g", cmap="coolwarm", cbar_kws={'label': 'Transition Count'})
-    plt.title(f"Transition Heatmap (A_bottom vs. A_top) in {genome.readable_name}")
-    plt.xlabel("A_top")
-    plt.ylabel("A_bottom")
-
-    if system() == "Darwin":
-        plt.show()
-    else:
-        plt.savefig(genome.plot_dir / "A_positions_switched.pdf", format="pdf")
-
-    # Plot the heatmap
-    plt.figure(figsize=(8, 6))
-    sns.heatmap(Ctransition_counts, annot=True, fmt="g", cmap="coolwarm", cbar_kws={'label': 'Transition Count'})
-    plt.title(f"Transition Heatmap (C_bottom vs. C_top) in {genome.readable_name}")
-    plt.xlabel("C_top")
-    plt.ylabel("C_bottom")
-    if system() == "Darwin":
-        plt.show()
-    else:
-        plt.savefig(genome.plot_dir / "C_positions_switched.pdf", format="pdf")
 
 
 def generate_possible_sequences(seq):
